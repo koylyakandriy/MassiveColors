3d3a169d3b118e297181db80fe0d384c215dd384
koylyakandriy <koylyakandriy@gmail.com>
2019-11-23 16:40:54 +0200

    Updates

---
+++ precache-manifest.643776d207d736bcf43675f44896d6ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5d868235a713bd0788630aaf234007",
+    "url": "/MassiveColors/index.html"
+  },
+  {
+    "revision": "f2e75dfec1c77ca48d41",
+    "url": "/MassiveColors/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "c71fe1f378a6357dc4cc",
+    "url": "/MassiveColors/static/css/main.560104f4.chunk.css"
+  },
+  {
+    "revision": "f2e75dfec1c77ca48d41",
+    "url": "/MassiveColors/static/js/2.be2a8dfe.chunk.js"
+  },
+  {
+    "revision": "c71fe1f378a6357dc4cc",
+    "url": "/MassiveColors/static/js/main.9f65743d.chunk.js"
+  },
+  {
+    "revision": "00de26e7609e72723f90",
+    "url": "/MassiveColors/static/js/runtime-main.733cc7c5.js"
+  },
+  {
+    "revision": "2243470685a1ab65357c52b5bfae42c3",
+    "url": "/MassiveColors/static/media/bg.22434706.svg"
+  }
+]);
